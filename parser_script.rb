5099823d56e0deee807a3467cf724f58b38acbdb
Boris <Boris0318@list.ru>
2019-10-18 15:04:11 +0300

    Update parser_script

--- parser_script.rb
+++ parser_script.rb
@@ -1,21 +1,19 @@
-# frozen_string_literal: true
-
-require_relative '../services/ser'
-require_relative '../services/ple_converter'
-require_relative '../services/unique_converter'
-require_relative '../services/format_transducer'
+require_relative 'services/parser'
+require_relative 'services/unique_visits_stats'
+require_relative 'services/all_visits_stats'
+require_relative 'services/printer'
 
 content = Parser.new
 content.file_reader(ARGV[0])
 content.info_parser
-statistics1 = SimpleConverter.new(content.information)
+statistics1 = AllVisitsStats.new(content.information)
 statistics1.convert
-statistics2 = UniqueConverter.new(content.information)
+statistics2 = UniqueVisitsStats.new(content.information)
 statistics2.convert
-results_for_all_statistics = FormatTransducer.new(statistics1.all_statistics)
-results_for_unique_statistics = FormatTransducer.new(statistics2.unique_statistics)
+results_all_statistics = Printer.new(statistics1.all_statistics)
+results_unique_statistics = Printer.new(statistics2.unique_statistics)
 
 puts 'All visits statistic ( host --- number of visits ):'
-results_for_all_statistics.show_formatted_statistics
+results_all_statistics.show_formatted_statistics
 puts "\nUnique visits statistic ( host --- number of unique visits ):"
-results_for_unique_statistics.show_formatted_statistics
+results_unique_statistics.show_formatted_statistics
